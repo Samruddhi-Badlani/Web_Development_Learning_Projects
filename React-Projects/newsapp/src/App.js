414ba1c3983d371ece1286fb159906d316ee0df3
Samruddhi-Badlani <samruddhi4227132@gmail.com>
2022-06-11 23:30:39 +0530

    Started with class based components in React

---
+++ React-Projects/newsapp/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+import React, { Component } from 'react'
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Hello Class Based React</h1>
+      </div>
+    )
+  }
+}
+

3403c6d3ebe54f6635617d692ae85fee4cfe0565
Samruddhi-Badlani <samruddhi4227132@gmail.com>
2022-06-12 18:25:27 +0530

    completed news item display using fetch api

--- React-Projects/newsapp/src/components/NewsItem.js
+++ React-Projects/newsapp/src/components/NewsItem.js
@@ -9,11 +9,11 @@ export class NewsItem extends Component {
     return (
       <div className="my-3">
         <div className="card" style={{width: "18rem"}}>
-          <img className="card-img-top" src={imageUrl} alt="Card image cap" />
+          <img className="card-img-top" src={imageUrl?imageUrl:"https://images.livemint.com/img/2022/06/12/600x338/ANI-20220406134-0_1655014392621_1655014414611.jpg"} alt="Card image cap" />
           <div className="card-body">
-            <h5 className="card-title">{title}</h5>
+            <h5 className="card-title">{title}...</h5>
             <p className="card-text">
-              {desc}
+              {desc}...
             </p>
             <a href={newsUrl} target="_blank" className="btn btn-primary btn-sm">
               Read More

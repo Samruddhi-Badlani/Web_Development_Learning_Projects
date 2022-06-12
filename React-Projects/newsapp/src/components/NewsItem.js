deb9189ab4ffcfd0eae75a484103366e000bbb8c
Samruddhi-Badlani <samruddhi4227132@gmail.com>
2022-06-12 14:02:32 +0530

    added the state to the news item component

--- React-Projects/newsapp/src/components/NewsItem.js
+++ React-Projects/newsapp/src/components/NewsItem.js
@@ -1,16 +1,28 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 export class NewsItem extends Component {
+
+    
   render() {
+
+    let {title,desc,imageUrl,newsUrl} = this.props;
     return (
-      <div>
-
-        This is a news item  component
-
-        
+      <div className="my-3">
+        <div className="card" style={{width: "18rem"}}>
+          <img className="card-img-top" src={imageUrl} alt="Card image cap" />
+          <div className="card-body">
+            <h5 className="card-title">{title}</h5>
+            <p className="card-text">
+              {desc}
+            </p>
+            <a href={newsUrl} target="_blank" className="btn btn-primary btn-sm">
+              Read More
+            </a>
+          </div>
+        </div>
       </div>
-    )
+    );
   }
 }
 
-export default NewsItem
+export default NewsItem;

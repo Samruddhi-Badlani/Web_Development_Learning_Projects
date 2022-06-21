2a8bf680481a3869ff74ef6345d2fe4e28d64e88
Samruddhi-Badlani <samruddhi4227132@gmail.com>
2022-06-22 01:06:54 +0530

    Create user completed

--- full-stack-project/enotebook/backend/routes/auth.js
+++ full-stack-project/enotebook/backend/routes/auth.js
@@ -1,16 +1,37 @@
 const express = require('express')
 const router = express.Router()
 const User = require('../models/User')
+const { body, validationResult } = require('express-validator');
 
 
 // Create a User using POST "/api/auth" . Does not require Auth
-router.post('/',(req,res)=>{
+router.post('/',[
+    body('email','Enter a valid email').isEmail(),
+  
+  body('name','Enter a valid name ').isLength({ min: 3 }),
+  body('password','Password must be of atleast 5 characters').isLength({ min: 5}),
+],(req,res)=>{
     console.log(req.body);
    
 
-    const user = User(req.body);
-    user.save();
-    res.send(req.body)
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    User.create({
+      name: req.body.name,
+      email : req.body.email,
+      
+      password: req.body.password,
+    }).then(user => res.json(user))
+    .catch(err =>{
+        console.log(err);
+        res.json({
+            error : "Email already registered!!",
+            message : err.message
+        })
+    });
 
 })
 

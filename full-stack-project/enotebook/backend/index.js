61a9d153959d6ef8edb751a678273e3c920b9f64
Samruddhi-Badlani <samruddhi4227132@gmail.com>
2022-06-25 17:22:49 +0530

    Completed the integration of add and delete api calls

--- full-stack-project/enotebook/backend/index.js
+++ full-stack-project/enotebook/backend/index.js
@@ -1,11 +1,13 @@
 const connectToMongo = require('./db');
 const express = require('express')
 const app = express()
+var cors = require('cors')
 const port = 5000;
 connectToMongo();
 
 
 app.use(express.json());
+app.use(cors())
 
 // Available Routes
 app.use('/api/auth',require('./routes/auth'))
